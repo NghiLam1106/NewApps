a5c4cbc69cd87fcb6413d337cd4550faee35a3f4
NghiLam1106 <lam91391@gmail.com>
2024-04-29 10:27:56 +0700

    dang nhap dang ky update 1.1

--- app/src/main/java/com/example/do_an_co_so_3/MainActivity.kt
+++ app/src/main/java/com/example/do_an_co_so_3/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.do_an_co_so_3.navigation.NavigationGraph
 import com.example.do_an_co_so_3.ui.theme.Do_an_co_so_3Theme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavigationGraph()
+
                 }
             }
         }
