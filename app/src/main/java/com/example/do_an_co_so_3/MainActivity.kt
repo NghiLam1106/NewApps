330b9ccda2519104ea718a98e5a0ab7328aba28f
Vohuong1084 <thihuongvo699@gmail.com>
2024-04-29 09:45:53 +0700

    OnBoarding

--- app/src/main/java/com/example/do_an_co_so_3/MainActivity.kt
+++ app/src/main/java/com/example/do_an_co_so_3/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.do_an_co_so_3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +12,44 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.example.do_an_co_so_3.presentation.onboarding.OnBoardingScreen
 import com.example.do_an_co_so_3.ui.theme.Do_an_co_so_3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            Do_an_co_so_3Theme {
+            Do_an_co_so_3Theme(
+                dynamicColor = false
+            ) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                        OnBoardingScreen()
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Do_an_co_so_3Theme {
-        Greeting("Android")
+        OnBoardingScreen()
     }
 }
